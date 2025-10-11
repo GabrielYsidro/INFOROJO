c73ca3f2084cdefebf77d8f3501d4e8e2b5dfb88
Felix-Miyahira <fmiyahira05@gmail.com>
2025-10-11 18:15:39 -0500

    cambios front conductor

--- front/app/(conductor)/index.tsx
+++ front/app/(conductor)/index.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Dimensions, Platform } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useRouter } from 'expo-router';
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import styles from './StylesIndex';
 import MapSection from '@/components/MapSection';
 
-export default function ClienteMenuPrincipal() {
+export default function ConductorMenuPrincipal() {
 
     const router = useRouter();
+
+    const handleNavigate = (path: any) => {
+        router.push(path);
+    }
+
     return (
         <View style={styles.container}>
             {/* Top Buttons */}
             <View style={styles.topButtonsContainer}>
-                <TouchableOpacity style={styles.topButton}>
-                    <Icon name="filter-variant" size={20} color="#fff" />
-                    <Text style={styles.topButtonText}>Filtros</Text>
+                <TouchableOpacity style={styles.topButton}
+                onPress = {() => handleNavigate("/(conductor)/EnviarReporteTrafico/EnviarReporteTrafico")}>
+                    <Icon name="check-circle-outline" size={20} color="#fff" />
+                    <Text style={styles.topButtonText}>Llegada</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.topButton}>
-                    <Icon name="bus" size={20} color="#fff" />
-                    <Text style={styles.topButtonText}>Viajes</Text>
+                <TouchableOpacity style={styles.topButton}
+                onPress = {() => handleNavigate("/(conductor)/EnviarReporteDesvio/EnviarReporteDesvio")}>
+                    <Icon name="alert-circle-outline" size={20} color="#fff" />
+                    <Text style={styles.topButtonText}>Desvío</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.topButton}
+                onPress = {() => handleNavigate("/(conductor)/ReporteFalla/ReporteFalla")}>
+                    <Icon name="close-circle-outline" size={20} color="#fff" />
+                    <Text style={styles.topButtonText}>Falla</Text>
                 </TouchableOpacity>
             </View>
 
@@ -28,18 +39,14 @@ export default function ClienteMenuPrincipal() {
 
             {/* Bottom Navigation */}
             <View style={styles.bottomNav}>
-                <TouchableOpacity style={styles.navButtonActive}
-                    onPress={async () => {
-                        await AsyncStorage.clear();
-                        console.log("AsyncStorage limpiado");
-                    }}>
+                <TouchableOpacity style={styles.navButtonActive}>
                     <Icon name="home" size={28} color="#fff" />
                     <Text style={styles.navButtonTextActive}>Menu Principal</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity
                     style={styles.navButton}
-                    onPress={() => router.push("/(cliente)/CuentaUsuario/CuentaUsuario")}
+                    onPress={() => router.push("/(conductor)/CuentaUsuario/CuentaUsuario")}
                 >
                     <Icon name="account-circle" size={28} color="#c62828" />
                     <Text style={styles.navButtonText}>Cuenta</Text>
