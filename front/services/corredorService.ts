ef9104ad40358885a8639e4121f2a4896baea94c
Sideron <66834276+Sideron@users.noreply.github.com>
2025-10-12 19:28:46 -0500

    Bus modal update

---
+++ front/services/corredorService.ts
@@ -0,0 +1,18 @@
+import Constants from "expo-constants";
+
+const API_URL_DEV = Constants.expoConfig?.extra?.API_URL_DEV;
+const API_URL_PROD = Constants.expoConfig?.extra?.API_URL_PROD;
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+export const API_URL = isDev ? API_URL_DEV : API_URL_PROD;
+
+export const getBusInfo = async (corredor_id:Number) => {
+    const res = await fetch(`${API_URL}/corredor/${corredor_id}`, {
+        method: 'GET',
+      });
+    
+      if (!res.ok) throw new Error('No se pudo obtener el usuario');
+    
+      return res.json();
+}
