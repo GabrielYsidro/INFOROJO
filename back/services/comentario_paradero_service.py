6ec47a16443b93231cf33df41fc41187140e8b6a
Sideron <66834276+Sideron@users.noreply.github.com>
2025-11-01 13:15:17 -0500

    Update comentario_paradero_service.py

--- back/services/comentario_paradero_service.py
+++ back/services/comentario_paradero_service.py
@@ -1,9 +1,18 @@
 from sqlalchemy.orm import Session
 from models.Paradero import Paradero
+from models.ComentarioUsuarioParadero import ComentarioUsuarioParadero
 
 class ComentarioParaderoService:
     def __init__(self, db: Session):
         self.db = db
     
+    def obtener_comentarios(self, id_paradero:int):
+        return self.db.query(ComentarioUsuarioParadero).filter(ComentarioUsuarioParadero.id_paradero==id_paradero).all()
+    
     def obtener_paradero_perfil(self, id_paradero:int):
-        return self.db.query(Paradero).filter(Paradero.id_paradero==id_paradero).first()
+        paradero = self.db.query(Paradero).filter(Paradero.id_paradero==id_paradero).first()
+        comentarios = self.obtener_comentarios(id_paradero)
+        return {
+            "paradero": paradero,
+            "comentarios": comentarios
+        }
