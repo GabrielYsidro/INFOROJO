8c071d6aa7c755a76e39d18b45d625383ebbbeed
GabrielYsidro <gawayspa2@gmail.com>
2025-11-09 16:11:31 -0500

    fix: expo token

--- front/app.config.js
+++ front/app.config.js
@@ -21,6 +21,7 @@ export default {
         "foregroundImage": "./assets/images/adaptive-icon.png",
         "backgroundColor": "#ffffff"
       },
+      "useNextNotificationsApi": true,
       "edgeToEdgeEnabled": true,
       "config": {
         "googleMaps": {
