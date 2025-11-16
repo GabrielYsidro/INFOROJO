cf5dcb2fcc51a474d82d44a09af00a78fc5a6231
Felix-Miyahira <fmiyahira05@gmail.com>
2025-11-16 14:09:16 -0500

    Update feedbackService.ts

--- front/services/feedbackService.ts
+++ front/services/feedbackService.ts
@@ -1,4 +1,11 @@
-import { API_URL } from "./ReporteService";
+import Constants from 'expo-constants';
+
+const API_URL_DEV = Constants.expoConfig?.extra?.API_URL_DEV;
+const API_URL_PROD = Constants.expoConfig?.extra?.API_URL_PROD;
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+export const API_URL = isDev ? API_URL_DEV : API_URL_PROD;
 
 export async function sendFeedback(body: Record<string, any>, options?: { xUserId?: string; token?: string }) {
   const url = `${API_URL}/feedback`;
